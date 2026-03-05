f1496bd910c907906040c1082ec4a6cf9d4a521d
Vadim Seleznov <vadimseleznov929@gmail.com>
2026-03-05 21:09:16 +0100

    scan_tokens first implementation

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,23 +1,87 @@
+use std::fmt::format;
+
 pub struct Scanner {
     source: String,
     tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
 }
 
 impl Scanner {
     pub fn new(source: &str) -> Self {
-        Self { source: source.to_string(), tokens: vec![] }
+        Self { source: source.to_string(), tokens: vec![], 
+            start: 0, current: 0, line: 1 }
     }
 
-    pub fn scan_tokens(&self) -> Result<Vec<Token>, String> {
-        todo!();
+    pub fn scan_tokens(&mut self) -> Result<Vec<Token>, String> {
+        while!self.is_at_end() {
+            self.start = self.current;
+            self.scan_token()?;
+        }
+
+        self.tokens.push(Token { 
+            token_type: TokenType::EOF, 
+            lexeme: "".to_string(), 
+            literal: None, 
+            line_number: self.line });
+        
+        Ok(self.tokens.clone())
+    }
+
+    fn scan_token(&mut self) -> Result<Token, String> {
+        let c = self.advance();
+
+        match c {
+            '(' => self.add_token(TokenType::LeftParen),
+            ')' => self.add_token(TokenType::RightParen),
+            '{' => self.add_token(TokenType::LeftBrace),
+            '}' => self.add_token(TokenType::RightBrace),
+            ',' => self.add_token(TokenType::Comma),
+            '.' => self.add_token(TokenType::Dot),
+            '-' => self.add_token(TokenType::Minus),
+            ';' => self.add_token(TokenType::Semicolon),
+            '+' => self.add_token(TokenType::Plus),
+            '*' => self.add_token(TokenType::Star),
+            '/' => self.add_token(TokenType::Slash),
+            _ => return Err(format!("Unrecognized char: {c}")),
+        }
+
+        todo!()
+    }
+
+    fn advance(&mut self) -> char {
+        let c = self.source.as_bytes()[self.current];
+        self.current += 1;
+
+        c as char
+    }
+
+    fn add_token(&mut self, token_type: TokenType) {
+        let mut text = String::new();
+        let bytes = self.source.as_bytes();
+        for i in self.start..self.current {
+            text.push(bytes[i] as char);
+        }
+
+        self.tokens.push(Token {
+            token_type,
+            lexeme: text,
+            literal: None,
+            line_number: self.line,
+        });
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum TokenType {
     // ONE CHAR TOKENS
     LeftParen, RightParen, LeftBrace, RightBrace, 
-    Comma, Dot, Minus, Plus, Semicolon, Slash, Start,
+    Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
 
     // ONE-TWO CHAR TOKENS AS ==
     Bang, BangEqual,
@@ -35,7 +99,7 @@ pub enum TokenType {
     EOF,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum LiteralType {
     IntValue(i64),
     FloatValue(f64),
@@ -43,16 +107,16 @@ pub enum LiteralType {
     IdentifierValue(String),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Token {
     token_type: TokenType,
     lexeme: String,
     literal: Option<LiteralType>,
-    line_number: u64,
+    line_number: usize,
 }
 
 impl Token {
-    pub fn new(token_type: TokenType, lexeme: String, literal: Option<LiteralType>, line_number: u64) -> Self {
+    pub fn new(token_type: TokenType, lexeme: String, literal: Option<LiteralType>, line_number: usize) -> Self {
         Self {
             token_type,
             lexeme,
